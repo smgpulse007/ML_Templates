9eaccca19ae1603a26331bb77c280ad963fa92f5
smgpulse007 <smgpulse007@gmail.com>
2024-01-18 15:32:14 -0800

    added imports

--- TokenizePrintEmoji.py
+++ TokenizePrintEmoji.py
@@ -1,3 +1,6 @@
+# Import
+from nltk.tokenize import regexp_tokenize
+
 # Tokenize and print only emoji
 emoji = "['\U0001F300-\U0001F5FF'|'\U0001F600-\U0001F64F'|'\U0001F680-\U0001F6FF'|'\u2600-\u26FF\u2700-\u27BF']"
 print(regexp_tokenize(text, emoji))
