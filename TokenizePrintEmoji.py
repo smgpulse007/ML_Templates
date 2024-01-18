ab93cc28369e0359402a5c078ad0f74fa0e25aa0
smgpulse007 <smgpulse007@gmail.com>
2024-01-18 15:30:29 -0800

    ONLY EMOJI

---
+++ TokenizePrintEmoji.py
@@ -0,0 +1,3 @@
+# Tokenize and print only emoji
+emoji = "['\U0001F300-\U0001F5FF'|'\U0001F600-\U0001F64F'|'\U0001F680-\U0001F6FF'|'\u2600-\u26FF\u2700-\u27BF']"
+print(regexp_tokenize(text, emoji))
